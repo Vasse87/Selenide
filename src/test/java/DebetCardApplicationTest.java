3f5d895cc91ecc8d4ed856e9668a78b25de2ddee
Sonya test <0404pmm@mail.ru>
2023-06-02 14:37:00 +0300

    Build Success

--- src/test/java/DebetCardApplicationTest.java
+++ src/test/java/DebetCardApplicationTest.java
@@ -1,11 +1,26 @@
 import com.codeborne.selenide.SelenideElement;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
 
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class DebetCardApplicationTest {
+
+    WebDriver driver;
+
+    @BeforeEach
+    void setup() {
+        driver = WebDriverManager.chromedriver().create();
+    }
+    @AfterEach
+    void teardown() {
+        driver.quit();
+    }
     @Test
     void test1() throws InterruptedException {
         open("http://localhost:9999");
