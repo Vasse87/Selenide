025282570dd3736c2a34475744102dcbf18951d6
Sonya test <0404pmm@mail.ru>
2023-06-02 15:03:32 +0300

    Test

--- src/test/java/DebetCardApplicationTest.java
+++ src/test/java/DebetCardApplicationTest.java
@@ -12,11 +12,23 @@ import static com.codeborne.selenide.Selenide.open;
 public class DebetCardApplicationTest {
 
     @Test
-    void test1() throws InterruptedException {
+    void shouldReturnPositiveResult() {
         open("http://localhost:9999");
         //Thread.sleep(10000000);
         SelenideElement form = $("[id = root]");
         form.$("[data-test-id = name] input").setValue("Васильев Сергей");
+        form.$("[data-test-id = phone] input").setValue("+79967923485");
+        form.$("[data-test-id = agreement]").click();
+        form.$("button").click();
+        $("[data-test-id = order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+
+    @Test
+    void shouldReturnPositiveResultWithHyphen()  {
+        open("http://localhost:9999");
+        //Thread.sleep(10000000);
+        SelenideElement form = $("[id = root]");
+        form.$("[data-test-id = name] input").setValue("Васильева-Татарирова Анна-Луиза");
         form.$("[data-test-id = phone] input").setValue("+79967923485");
         form.$("[data-test-id = agreement]").click();
         form.$("button").click();
